f50d4c30b7a6d3adefd78a270a9456e9cd6f8adb
radmoparker <113465757+radmoparker@users.noreply.github.com>
2023-12-25 14:20:22 +0100

    adding_index
    refractor Connexion which use lbmaWebsite.php instead of index.html and ch.php to index.htmlanging lbmaWebsite

--- Connexion.php
+++ Connexion.php
@@ -8,7 +8,7 @@
 		//changement de la couleur du fond du dernier paragraphe
 		//Au clique du bouton "Cliquez"
 		function accueil(){
-			document.location.href ="lbmaWebsite.php?";
+			document.location.href ="index.html?";
 			
 			//document.fgColor = "red";
 		}
